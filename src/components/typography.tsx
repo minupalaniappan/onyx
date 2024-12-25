4e725e00fd0d91d66d6667556dd587e9679e00c4
Minu Palaniappan <minupal6@gmail.com>
2024-12-24 19:47:52 -0800

    gpf

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -1,6 +1,8 @@
+import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
 import { tw } from '@minupalaniappan/brise'
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
+import { Row } from './layout'
 
 export const CardHeader = tw.div<PropsWithChildren>`
   font-normal
@@ -24,14 +26,19 @@ export const PaginatorText = tw.div<PropsWithChildren>`
   whitespace-nowrap
 `
 
-export const ButtonLink = tw(Link)`
-  font-light
-  text-md
-  text-gray-600
-  leading-none
-  whitespace-nowrap
-  hover:underline
-`
+export const ButtonLink = (props: PropsWithChildren & { href: string }) => {
+  return (
+    <Row y="center" className="gap-[2px]">
+      <Link
+        className="font-light text-md text-gray-600 leading-none whitespace-nowrap hover:underline"
+        href={props.href}
+      >
+        {props.children}
+      </Link>
+      <ArrowUpRightIcon className="w-3 h-3 stroke  stroke-[3px] stroke-gray-600 relative bottom-[1px]" />
+    </Row>
+  )
+}
 
 export const OverlayHeader = tw.div<PropsWithChildren>`
   font-normal

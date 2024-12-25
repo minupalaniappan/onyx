183966177f321d4f2ca57dde49f5d72cf988dc41
Minu Palaniappan <minupal6@gmail.com>
2024-12-24 21:01:47 -0800

    gpf

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,8 @@ import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
+import Spinner from '../spinner'
+import { Row } from '../layout'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center gap-2 whitespace-nowrap text-lg font-light transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 w-full',
@@ -41,7 +43,17 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
-    return (
+    return props.isLoading ? (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        <Row grow y="center" x="center" className="h-[28px]">
+          <Spinner />
+        </Row>
+      </Comp>
+    ) : (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}

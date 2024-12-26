83468961ec93e16b25fe41d26023a12331ebe564
Minu Palaniappan <minupal6@gmail.com>
2024-12-25 21:55:12 -0800

    gpf

--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -1,11 +1,27 @@
 import type { Meta } from '@storybook/react'
 
 import Tag from '../components/tag'
-import { Tooltip } from '../components/ui/tooltip'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../components/ui/tooltip'
 
 const meta: Meta<typeof Tooltip> = {
   title: 'Components/Tooltip',
-  render: (args) => <Tag {...args} />,
+  render: () => (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>
+          <Tag>Trigger</Tag>
+        </TooltipTrigger>
+        <TooltipContent align="center" side="right">
+          <p>Tooltip content</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  ),
 }
 
 export default meta
